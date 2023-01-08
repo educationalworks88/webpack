3f0bf6476a93dd666765e24028cb0be9e205bb28
educationalworks88 <98517338+educationalworks88@users.noreply.github.com>
2023-01-08 18:54:14 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// CSS一式を読み込んでパッケージ
+import 'bootstrap/dist/css/bootstrap.min.css';
+// 独自CSS用ファイル
+import "./css/style.css";
+
+// JS一式を読み込んでパッケージ
+import 'bootstrap';
+// 独自JS用ファイル
+import './js/script.js';
